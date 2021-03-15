e3becfffc232dc53a908b7e30d8106c88ec7cf18
j-walrath <jwalrath5@gmail.com>
2021-03-15 15:53:13 -0400

    Added pure pose control, removed pose control w/ collision avoidance (changed method signature)

--- control.py
+++ control.py
@@ -4,23 +4,23 @@ from scipy.linalg import logm
 
 import pybullet as pb
 
-DEG_TO_RAD = np.pi/180
+DEG_TO_RAD = np.pi / 180
 
 
 def euler_to_rot_mat(yaw):
     return np.array([[np.cos(yaw), -np.sin(yaw)],
-                      [np.sin(yaw), np.cos(yaw)]])
+                     [np.sin(yaw), np.cos(yaw)]])
 
 
 def normalize_vector(v):
-    return v/np.linalg.norm(v)
+    return v / np.linalg.norm(v)
 
 
 def compute_rotational_velocity(yaw_robot, yaw_target):
     relative_orientation = yaw_target - yaw_robot
 
     relative_orientation_mat = np.array([[np.cos(relative_orientation), -np.sin(relative_orientation)],
-                                          [np.sin(relative_orientation), np.cos(relative_orientation)]])
+                                         [np.sin(relative_orientation), np.cos(relative_orientation)]])
 
     return logm(relative_orientation_mat)[1, 0]
 
@@ -37,7 +37,8 @@ class RobotControl:
         self.max_rotational_velocity = max_rotational_velocity
 
     def get_manipulator_state(self, robot_id):
-        joint_angles, velocity, _, _ = zip(*self.pb_client.getJointStates(bodyUniqueId=robot_id, jointIndices=range(11-4, 15-4)))
+        joint_angles, velocity, _, _ = zip(
+            *self.pb_client.getJointStates(bodyUniqueId=robot_id, jointIndices=range(11 - 4, 15 - 4)))
 
         return (joint_angles, velocity)
 
@@ -53,11 +54,14 @@ class RobotControl:
 
     def gripper_control(self, robot_id, gripper_target_state):
         for i, value in enumerate(gripper_target_state):
-            self.pb_client.setJointMotorControl2(bodyUniqueId=robot_id, jointIndex=15+i, controlMode=pb.POSITION_CONTROL, targetPosition=value)
+            self.pb_client.setJointMotorControl2(bodyUniqueId=robot_id, jointIndex=15 + i,
+                                                 controlMode=pb.POSITION_CONTROL, targetPosition=value)
 
     def manipulator_control(self, robot_id, arm_target_state):
         for i, value in enumerate(arm_target_state):
-            self.pb_client.setJointMotorControl2(bodyUniqueId=robot_id, jointIndex=11+i-4, controlMode=pb.POSITION_CONTROL, targetPosition=value, maxVelocity=5.0*2)
+            self.pb_client.setJointMotorControl2(bodyUniqueId=robot_id, jointIndex=11 + i - 4,
+                                                 controlMode=pb.POSITION_CONTROL, targetPosition=value,
+                                                 maxVelocity=5.0 * 2)
 
     def gripper_pinch(self, robot_id):
         gripper_target_state = (-0.016812667692005963, -0.01000778853017392)
@@ -70,7 +74,7 @@ class RobotControl:
         self.gripper_control(robot_id, gripper_target_state)
 
     def manipulator_origin(self, robot_id):
-        arm_target_state = (DEG_TO_RAD*0, DEG_TO_RAD*-80, DEG_TO_RAD*60, DEG_TO_RAD*30)
+        arm_target_state = (DEG_TO_RAD * 0, DEG_TO_RAD * -80, DEG_TO_RAD * 60, DEG_TO_RAD * 30)
 
         self.manipulator_control(robot_id, arm_target_state)
 
@@ -80,13 +84,13 @@ class RobotControl:
         self.manipulator_control(robot_id, arm_target_state)
 
     def manipulator_place(self, robot_id):
-        arm_target_state = (DEG_TO_RAD*180-.2*np.random.random(), 0, .4, 1.1)
+        arm_target_state = (DEG_TO_RAD * 180 - .2 * np.random.random(), 0, .4, 1.1)
 
         self.manipulator_control(robot_id, arm_target_state)
 
     def velocity_control(self, robot_id, linear_velocity, rotational_velocity):
-        left_wheel_velocity = linear_velocity/0.033 - rotational_velocity*0.027135999999999997/(.144*.033)
-        right_wheel_velocity = linear_velocity/0.033 + rotational_velocity*0.027135999999999997/(.144*.033)
+        left_wheel_velocity = linear_velocity / 0.033 - rotational_velocity * 0.027135999999999997 / (.144 * .033)
+        right_wheel_velocity = linear_velocity / 0.033 + rotational_velocity * 0.027135999999999997 / (.144 * .033)
 
         self.pb_client.setJointMotorControl2(bodyUniqueId=robot_id,
                                              jointIndex=0,
@@ -117,8 +121,8 @@ class RobotControl:
     def get_closest_point(self, robot1_id, robot2_id, robot1_pose, distance):
         closest_points = self.pb_client.getClosestPoints(bodyA=robot1_id, bodyB=robot2_id, distance=distance)
 
-        distances_to_points = [euclidean_norm((point[5][0]-point[6][0], point[5][1]-point[6][1]))
-                               for point in closest_points if point[3] not in [0,1] and point[4] not in [0,1]]
+        distances_to_points = [euclidean_norm((point[5][0] - point[6][0], point[5][1] - point[6][1]))
+                               for point in closest_points if point[3] not in [0, 1] and point[4] not in [0, 1]]
 
         if len(distances_to_points) == 0:
             return []
@@ -126,21 +130,21 @@ class RobotControl:
         closest_point_index = np.argmin(distances_to_points)
         return closest_points[closest_point_index][6]
 
-    def pose_control_with_collision_avoidance(self, robot_id, destination, avoidance_direction, weight_magnitude, weight_degree, threshold_distance=2.0):
+    def pose_control(self, robot_id, destination):
         robot_pose, robot_velocity = self.get_robot_state(robot_id=robot_id)
 
-        vector_to_destination = (destination[0]-robot_pose[0],
-                                 destination[1]-robot_pose[1])
+        vector_to_destination = (destination[0] - robot_pose[0],
+                                 destination[1] - robot_pose[1])
         unit_vector_to_destination = normalize_vector(vector_to_destination)
         unit_vector_yaw = np.array((np.cos(robot_pose[2]), np.sin(robot_pose[2])))
 
         correlation_coefficient = np.dot(unit_vector_yaw, unit_vector_to_destination)
         distance_to_destination = euclidean_norm(vector_to_destination)
 
-        linear_velocity = min(self.max_linear_velocity, correlation_coefficient*distance_to_destination)
+        linear_velocity = min(self.max_linear_velocity, correlation_coefficient * distance_to_destination)
         linear_velocity = max(self.minimum_linear_velocity, linear_velocity)
 
-        orientation_to_destination = np.arctan2(destination[1]-robot_pose[1], destination[0]-robot_pose[0])
+        orientation_to_destination = np.arctan2(destination[1] - robot_pose[1], destination[0] - robot_pose[0])
         rotational_velocity = compute_rotational_velocity(robot_pose[2], orientation_to_destination)
 
         rotational_velocity = min(self.max_rotational_velocity, rotational_velocity)
@@ -151,24 +155,24 @@ class RobotControl:
         return linear_velocity, rotational_velocity
 
     def pose_control_oa_v1(self, robot_id, pose_dest, pose_robot, laser_scan_data=[], gain_oa=2.0, dist_threshold=.8):
-        vec_to_dest = (pose_dest[0]-pose_robot[0],
-                           pose_dest[1]-pose_robot[1])
+        vec_to_dest = (pose_dest[0] - pose_robot[0],
+                       pose_dest[1] - pose_robot[1])
         unit_vec_to_dest = normalize_vector(vec_to_dest)
         unit_yaw_vec = np.array((np.cos(pose_robot[2]), np.sin(pose_robot[2])))
 
         cor_coeff = np.dot(unit_yaw_vec, unit_vec_to_dest)
         dist_to_dest = np.linalg.norm(vec_to_dest)
-        linear_vel = min(self.max_linear_velocity, 1.0*cor_coeff*dist_to_dest)
+        linear_vel = min(self.max_linear_velocity, 1.0 * cor_coeff * dist_to_dest)
         linear_vel = max(0.05, linear_vel)
 
-        yaw_target = np.arctan2(pose_dest[1]-pose_robot[1], pose_dest[0]-pose_robot[0])
+        yaw_target = np.arctan2(pose_dest[1] - pose_robot[1], pose_dest[0] - pose_robot[0])
         rot_vec = compute_rotational_velocity(pose_robot[2], yaw_target)
 
-        ## process laser scan data
+        # process laser scan data
         for data in laser_scan_data:
             x_b, y_b, _ = data
 
-            vec_to_data_point = np.array((x_b-pose_robot[0], y_b-pose_robot[1]))
+            vec_to_data_point = np.array((x_b - pose_robot[0], y_b - pose_robot[1]))
             dist_to_data_point = np.linalg.norm(vec_to_data_point)
             unit_vec_to_data_point = normalize_vector(vec_to_data_point)
 
@@ -177,28 +181,31 @@ class RobotControl:
 
             if dist_to_data_point <= .4: linear_vel = 0.02
 
-            weight = np.exp(-gain_oa*(dist_to_data_point-dist_threshold))
-            rot_direction = np.sign(np.cross((unit_vec_to_data_point[0], unit_vec_to_data_point[1], 0), (unit_yaw_vec[0], unit_yaw_vec[1], 0))[2])
-            yaw_target = np.arctan2(unit_vec_to_data_point[1], unit_vec_to_data_point[0]) + rot_direction*np.pi/2
-            rot_vec += weight*compute_rotational_velocity(pose_robot[2], yaw_target)
+            weight = np.exp(-gain_oa * (dist_to_data_point - dist_threshold))
+            rot_direction = np.sign(np.cross((unit_vec_to_data_point[0], unit_vec_to_data_point[1], 0),
+                                             (unit_yaw_vec[0], unit_yaw_vec[1], 0))[2])
+            yaw_target = np.arctan2(unit_vec_to_data_point[1], unit_vec_to_data_point[0]) + rot_direction * np.pi / 2
+            rot_vec += weight * compute_rotational_velocity(pose_robot[2], yaw_target)
 
-        rot_vel = min(self.max_rotational_velocity, 1.0*rot_vec)
+        rot_vel = min(self.max_rotational_velocity, 1.0 * rot_vec)
         rot_vel = max(-self.max_rotational_velocity, rot_vel)
-    
+
         self.velocity_control(robot_id, linear_vel, rot_vel)
 
         return linear_vel, rot_vel
 
-    ## Visual servoing for object pickup
+    # Visual servoing for object pickup
     def visual_servoing(self, robot_id, target_position, pose):
-        distance_to_target = np.linalg.norm((target_position[0]-pose[0], target_position[1]-pose[1])) - self.object_distance_offset
-        orientation_to_target = np.arctan2(target_position[1]-pose[1], target_position[0]-pose[0]) - pose[2]
+        distance_to_target = np.linalg.norm(
+            (target_position[0] - pose[0], target_position[1] - pose[1])) - self.object_distance_offset
+        orientation_to_target = np.arctan2(target_position[1] - pose[1], target_position[0] - pose[0]) - pose[2]
 
-        rotational_velocity = min(self.max_rotational_velocity, compute_rotational_velocity(pose[2], np.arctan2(target_position[1]-pose[1], target_position[0]-pose[0])))
+        rotational_velocity = min(self.max_rotational_velocity, compute_rotational_velocity(pose[2], np.arctan2(
+            target_position[1] - pose[1], target_position[0] - pose[0])))
         rotational_velocity = max(-self.max_rotational_velocity, rotational_velocity)
 
         if np.abs(rotational_velocity) < 0.2:
-            linear_velocity = min(self.max_linear_velocity, 1.0*distance_to_target)
+            linear_velocity = min(self.max_linear_velocity, 1.0 * distance_to_target)
             linear_velocity = max(-self.max_linear_velocity, linear_velocity)
 
         else:
