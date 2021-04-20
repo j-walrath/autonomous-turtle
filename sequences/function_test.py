6dbd97d813e9dc14861baa1c916a71e07b88275c
j-walrath <jwalrath5@gmail.com>
2021-04-20 11:22:30 -0400

    Update to use library functions.

--- sequences/function_test.py
+++ sequences/function_test.py
@@ -2,15 +2,10 @@ import time
 import logging
 from utils.control import RobotControl
 from utils import fsm
+import utils.simulator_library as lib
 
 CONTROL_FREQUENCY = 40
 RADIUS = 1
-
-
-def wait(pb, t):
-    for _ in range(t):
-        pb.stepSimulation()
-        time.sleep(1. / 240.)
 
 
 def function_test(pb, objects, object_states, robots, robot_fsms):
@@ -27,6 +22,14 @@ def function_test(pb, objects, object_states, robots, robot_fsms):
 
     cells = [(4, 4), (4, -4), (-4, -4), (-4, 4)]
     targets = [(3.5, 3.5), (3.5, -3.5), (-3.5, -3.5), (-3.5, 3.5)]
+    coords = [(-3.8, -3.8), (-3.8, -3.6), (-3.8, -3.4), (-3.6, -3.8), (-3.4, -3.8),
+              (-3.8, 3.8), (-3.8, 3.6), (-3.8, 3.4), (-3.6, 3.8), (-3.4, 3.8),
+              (3.8, 3.8), (3.8, 3.6), (3.8, 3.4), (3.6, 3.8), (3.4, 3.8),
+              (3.8, -3.8), (3.8, -3.6), (3.8, -3.4), (3.6, -3.8), (3.4, -3.8)]
+
+    for obj in lib.load_objects(pb, coords):
+        objects.append(obj)
+        object_states[obj] = "ON_GROUND"
 
     # MEASURE CELLS
     for i in range(len(cells)):
@@ -39,16 +42,16 @@ def function_test(pb, objects, object_states, robots, robot_fsms):
 
             robot_fsm.run_once((manipulator_state, robot_state))
 
-            wait(pb, int(240 / CONTROL_FREQUENCY))
+            lib.step(pb, int(240 / CONTROL_FREQUENCY))
 
             if robot_fsm.current_state == "NONE":
-                wait(pb, 100)
+                lib.step(pb, 100)
 
                 m = controller.measure(robot, robot_fsm.obj_states, r=RADIUS, noise="GAUSSIAN")
                 actual = controller.measure(robot, robot_fsm.obj_states, r=RADIUS)
                 logging.debug("Robot measured {} objects (Actual = {}).".format(m, actual))
 
-                wait(pb, 100)
+                lib.step(pb, 100)
 
                 robot_fsm.set_destination((0, 0))
                 logging.debug("Returning to Origin...")
@@ -58,7 +61,7 @@ def function_test(pb, objects, object_states, robots, robot_fsms):
 
                     robot_fsm.run_once((manipulator_state, robot_state))
 
-                    wait(pb, int(240 / CONTROL_FREQUENCY))
+                    lib.step(pb, int(240 / CONTROL_FREQUENCY))
 
                     if robot_fsm.current_state == "NONE":
                         break
@@ -77,7 +80,7 @@ def function_test(pb, objects, object_states, robots, robot_fsms):
 
             robot_fsm.run_once((manipulator_state, robot_state))
 
-            wait(pb, int(240 / CONTROL_FREQUENCY))
+            lib.step(pb, int(240 / CONTROL_FREQUENCY))
 
             if robot_fsm.current_state == "NONE":
 
@@ -90,13 +93,13 @@ def function_test(pb, objects, object_states, robots, robot_fsms):
                 #
                 #     robot_fsm.run_once((manipulator_state, robot_state))
                 #
-                #     wait(pb, int(240 / CONTROL_FREQUENCY))
+                #     lib.step(pb, int(240 / CONTROL_FREQUENCY))
                 #
                 #     if robot_fsm.current_state == "NONE":
                 #         break
                 break
 
-    wait(pb, 100)
+    lib.step(pb, 100)
 
     logging.debug("Returning to Main...")
 
