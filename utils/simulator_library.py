461d2d6470070a7d091ddbd0cb507ae9005466e6
j-walrath <jwalrath5@gmail.com>
2021-04-20 20:56:02 -0400

    Enabled caching when loading objects (necessary for loading many robots).

--- utils/simulator_library.py
+++ utils/simulator_library.py
@@ -1,4 +1,5 @@
 import time
+import pybullet as p
 from utils.fsm import RobotStateMachine
 from utils.control import RobotControl
 
@@ -22,12 +23,13 @@ def load_plane(pb, position=None, lateralFriction=3.0, spinningFriction=0.03, ro
     return plane
 
 
-def load_objects(pb, locations, COLLISION=False):
+def load_objects(pb, locations, collision=False):
     objects = []
     for loc in locations:
-        objects.append(pb.loadURDF(OBJ_MODEL, basePosition=[loc[0], loc[1], 0.3], globalScaling=1.0))
+        objects.append(pb.loadURDF(OBJ_MODEL, basePosition=[loc[0], loc[1], 0.3], globalScaling=1.0,
+                                   flags=p.URDF_ENABLE_CACHED_GRAPHICS_SHAPES))
 
-        if not COLLISION:
+        if not collision:
             # Do not collide with robots or other objects
             pb.setCollisionFilterGroupMask(objects[-1], -1, 0, 0)
 
@@ -37,13 +39,23 @@ def load_objects(pb, locations, COLLISION=False):
     return objects
 
 
-def load_robots(pb, locations):
+def load_robots(pb, locations, collision=False):
     robots = []
     orn = pb.getQuaternionFromEuler([0, 0, 0])
     for loc in locations:
-        robots.append(pb.loadURDF(ROBOT_MODEL, basePosition=[loc[0], loc[1], 0.5], baseOrientation=orn))
+        robots.append(pb.loadURDF(ROBOT_MODEL, basePosition=[loc[0], loc[1], 0.5], baseOrientation=orn,
+                                  flags=p.URDF_ENABLE_CACHED_GRAPHICS_SHAPES))
         pb.changeDynamics(robots[-1], -1, maxJointVelocity=300, lateralFriction=1.0, rollingFriction=0.03,
                           restitution=0.7)
+        step(pb, 100)
+
+        if not collision:
+            # Do not collide with other robots
+            pb.setCollisionFilterGroupMask(robots[-1], -1, 0, 0)
+
+            # Do collide with the ground plane
+            pb.setCollisionFilterPair(robots[-1], 0, -1, -1, 1)
+
     return robots
 
 
