6762e9e5680244c14bb31229d382bcc35f4f75de
j-walrath <jwalrath5@gmail.com>
2021-04-24 23:31:13 -0400

    Updated default agent parameters to reflect differentially driven case.

--- utils/simulator_library.py
+++ utils/simulator_library.py
@@ -113,9 +113,9 @@ def get_agent(robot_id, pose, v):
     agent.id_ = robot_id
     agent.max_neighbors_ = 10
     agent.max_speed_ = utils.control.MAX_LINEAR_V
-    agent.neighbor_dist_ = 2.0
-    agent.radius_ = 0.3
-    agent.time_horizon_ = 3.0
+    agent.neighbor_dist_ = 5.0
+    agent.radius_ = utils.control.RADIUS * 2
+    agent.time_horizon_ = 4.0
     agent.time_horizon_obst_ = 10.0
     agent.time_step_ = 1 / CONTROL_FREQUENCY
 
